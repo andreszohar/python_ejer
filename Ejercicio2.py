786cd5a47b6232a56b0c85779a2fae5001409c49
andreszohar <71178680+andreszohar@users.noreply.github.com>
2021-03-12 13:34:48 -0500

    Resueltos
    Resueltos 3 ejer

---
+++ Ejercicio2.py
@@ -0,0 +1,12 @@
+#Un joven desea matricularse en una escuela de natación, se le solicita el nombre, la edad, el teléfono, muestre los valores ingresados.
+
+nombre=input("Introduce el Nombre: ")
+edad=input("Introduce la edad: ")
+telefono=input("Introduce el teléfono: ")
+
+
+
+print("Los datos registrados son:")
+print("Nombre: " ,nombre)
+print("Edad: " ,edad)
+print("Teléfono: " ,telefono)
